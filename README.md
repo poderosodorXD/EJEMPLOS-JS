4634cb6c3470425059092085fe63e9ccd1e79ffe
poderosodorXD <78448414+poderosodorXD@users.noreply.github.com>
2023-05-21 01:27:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EJEMPLOS-JS
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-v193dg)
---
+++ index.js
@@ -0,0 +1,11 @@
+// Import stylesheets
+import './style.css';
+// Import Jquery
+import $ from 'jquery';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starterhhhhhhhhh</h1>`;
+
+var textoParrafo = $("#TEXTO").text();
+console.log("Jquery: ",textoParrafo);
